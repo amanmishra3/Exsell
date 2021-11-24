5ceb970ac084d79900334593b3850dc7e8cd0581
tanmayk25 <tanmay7khandelwal@gmail.com>
2021-11-24 11:41:33 -0800

    Update Categories.java

--- app/src/main/java/com/android/exsell/UI/Categories.java
+++ app/src/main/java/com/android/exsell/UI/Categories.java
@@ -55,6 +55,7 @@ public class Categories extends AppCompatActivity {
             public void onClick(View v) {
                 drawer.openDrawer(GravityCompat.START);
             }
+            
         });
         loadProducts();
         categoryRecycler = findViewById(R.id.recyclerViewCategoryTiles);
